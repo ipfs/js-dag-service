d7c615068ff3c156a537e5a8c85b09438d756101
Carson Farmer <carson.farmer@gmail.com>
2019-11-07 15:51:29 -0800

    test: updates tests
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- tests/files.test.ts
+++ tests/files.test.ts
@@ -1,12 +1,10 @@
 import { createReadStream, promises as fs } from 'fs'
 import { MemoryDatastore } from 'interface-datastore'
-import exporter from 'ipfs-unixfs-exporter'
-import importer from 'ipfs-unixfs-importer'
-import { Peer, setupLibP2PHost, BlockStore } from '../src'
+import { Peer, setupLibP2PHost, BlockStore, Result } from '../src'
 
 const sleep = (ms: number) => new Promise(r => setTimeout(r, ms))
 let lite: Peer
-const results: any[] = []
+let root: Result | undefined
 
 describe('getting and putting files', () => {
   beforeAll(async () => {
@@ -22,36 +20,23 @@ describe('getting and putting files', () => {
   })
 
   it('read file from disc and put to "network"', async () => {
-    // `importer` chunks and adds content to the DAG service from a reader.
-    // The content is stored as a UnixFS DAG (default for IPFS).
-    // It returns the root IPLD node.
     await fs.writeFile('bar.txt', 'Hello World')
     const source = [
       {
-        path: '/tmp/foo/bar.txt',
+        path: 'bar',
         content: createReadStream('bar.txt'),
       },
     ]
-    for await (const entry of importer(source, lite, { cidVersion: 1, codec: 'dag-pb' })) {
-      console.log(entry.cid.toString())
-      results.push(entry)
-    }
+    root = await lite.addFile(source)
     // `result` should be the root DAG node
-    const str = 'bafybeibvxlf7xydshz54ri6bmlrc3h32old6oepldoro2tllvaj3a2o77a'
-    expect(results[results.length - 1].cid.toString()).toEqual(str)
+    const str = 'bafkreiffsgtnic7uebaeuaixgph3pmmq2ywglpylzwrswv5so7m23hyuny'
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    expect(root!.cid.toString()).toEqual(str)
   })
 
   it('get block from "network" and recursively export', async () => {
-    const bufs = []
-    const entry = await exporter(results[0].cid.toString(), lite)
-    if (entry.content) {
-      for await (const buf of entry.content()) {
-        bufs.push(buf as Buffer)
-      }
-    }
-    const content = Buffer.concat(bufs)
-
-    console.info(content)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const content = await lite.getFile(root!.cid.toString())
     expect(content.toString()).toEqual('Hello World')
   })
 })
