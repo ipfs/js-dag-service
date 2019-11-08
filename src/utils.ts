fb803b7d2342497b24213c88232b5d4ddc274244
Carson Farmer <carson.farmer@gmail.com>
2019-11-07 17:22:21 -0700

    addFile and getFile APIs (#9)
    * feat: add and get files apis
    
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>
    
    * test: updates tests
    
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>
    
    * test: update to generic port
    
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,11 @@ import { promisify } from 'es6-promisify'
 import Protector from 'libp2p/src/pnet'
 import PeerId from 'peer-id'
 import PeerInfo from 'peer-info'
+import Ipld from 'ipld'
+import CID from 'cids'
+import importer, { Options as AddOptions, File, Result } from 'ipfs-unixfs-importer'
+import exporter, { Options as GetOptions } from 'ipfs-unixfs-exporter'
+
 import { Node } from './bundle'
 
 export const setupLibP2PHost = async (
@@ -26,4 +31,32 @@ export const setupLibP2PHost = async (
     options.modules.connProtector = new Protector(secret)
   }
   return new Node(options)
+}
+
+export { Options as AddOptions, File, Result } from 'ipfs-unixfs-importer'
+
+export async function addFile(source: Iterable<File>, ipld: Ipld, options?: AddOptions) {
+  let result: Result | undefined
+  for await (const entry of importer(source, ipld, { cidVersion: 1, codec: 'dag-pb', ...options })) {
+    result = entry
+  }
+  return result
+}
+
+export { Options as GetOptions } from 'ipfs-unixfs-exporter'
+
+export async function getFile(cid: CID | Buffer | string, ipld: Ipld, options?: GetOptions) {
+  const file = await exporter(cid, ipld)
+  // File may not have unixfs prop if small & imported with rawLeaves true
+  if (file.unixfs && file.unixfs.type.includes('dir')) {
+    throw new Error('this dag node is a directory')
+  }
+  if (!file.content) {
+    throw new Error('this dag node has no content')
+  }
+  const arr: Buffer[] = []
+  for await (const entry of file.content(options)) {
+    arr.push(entry as Buffer)
+  }
+  return Buffer.concat(arr)
 }
